38bb9081965960e0e3c6b49e994e661973fb7f01
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-08-21 23:18:50 -0300

    Criado conexao com banco de dados

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-    echo "Boa Noite!";
+    //echo "Boa Noite!";
+    $con = mysqli_connect(
+    "localhost",
+    "root",
+    "123odeilso@", 
+    "syspacientes");
+    if(!$con){
+        echo "Error: ".PHP_EOL;
+        exit;
+    }
+    echo "Conectou!!!";
+    echo "<br>";
+    echo "Informações do host: " . 
+        mysqli_get_host_info($con);
+    
+    mysqli_close($con);
 ?>
