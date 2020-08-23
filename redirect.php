6c2bf6c98ca73bc2575b9f7831ddca2c3901c676
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-08-23 13:13:30 -0300

    Validação de senha e retorno

--- redirect.php
+++ redirect.php
@@ -1,16 +1,26 @@
 <?php
     $email = $_POST["inputEmail"];
     $senha = $_POST["inputPassword"];
+    $validou = true;
 
     //Validar Senha
     if(strlen($senha) < 6)
     {
-        echo "Senha menor que 6 caracteres";
+        //echo "Senha menor que 6 caracteres";
+        $validou = false;
     }else if(strlen($senha) >= 7)
     {
-        echo "Senha maior que 6 caracteres";
+        //echo "Senha maior que 6 caracteres";
+        $validou = false;
+    } 
+    
+    //Exibir ou Retornar
+    if($validou)
+    {
+        echo "<hr>";
+        echo "E-mail: ".$email."<br>";
+        echo "Senha: ".$senha;
+    }else{
+        header("location:index.php");
     }
-    echo "<hr>";
-    echo "E-mail: ".$email."<br>";
-    echo "Senha: ".$senha;
 ?>
