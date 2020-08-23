2ca7d659d8962e54ffce991efcbbdac9c2570d76
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-08-23 12:34:46 -0300

    Criando página de login

--- index.php
+++ index.php
@@ -22,7 +22,36 @@
     // $_GET pega o que passou pela URL
     // $_POST pega informação dos componentes das paginas
 
-    $nome = $_GET["nome"];
+    /*$nome = $_GET["nome"];
     $sobrenome = $_GET["sobrenome"];
-    echo "O Nome é: ".$nome." ".$sobrenome;
+    echo "O Nome é: ".$nome." ".$sobrenome;*/
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SysPacientes</title>
+    <link rel="icon" href="img/favicon/favicion1.ico">
+    
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="login.css" rel="stylesheet">
+</head>
+<body class="text-center">
+    <form class="form-signin">
+      <img class="mb-4" src="img/login.png" alt="" width="80" height="80">
+      <h1 class="h3 mb-3 font-weight-normal">Insira suas credenciais</h1>
+      <label for="inputEmail" class="sr-only">Endereço de E-mail</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Endereço de E-Mail" required autofocus>
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+      
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      <p class="mt-5 mb-3 text-muted">Odeilso&copy; 2017-2018</p>
+    </form>
+  </body>
+</html>
