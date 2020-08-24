649f59c6a6f5ab1aa3dea5fa0e81af82ec4f943c
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-08-23 22:36:16 -0300

    Exercicio teste5

--- admin.php
+++ admin.php
@@ -91,9 +91,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/bootstrap.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
