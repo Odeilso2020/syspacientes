2c2e1570ee3f103b5a755ceaa723e4f01a0cd4ba
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-08-23 23:06:53 -0300

    teste 1

--- admin.php
+++ admin.php
@@ -26,6 +26,8 @@
     
     <link href="css/bootstrap.min.css" rel="stylesheet"> <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
     <link href="css/navbar.css" rel="stylesheet">
     <!-- Fim -->
     
