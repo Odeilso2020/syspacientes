01c138ac71b344e15bd82286c05ce0c9b0715038
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-08-23 22:45:40 -0300

    Exercicio teste9

--- admin.php
+++ admin.php
@@ -76,7 +76,10 @@
 
     <!-- Main component for a primary marketing message or call to action -->
     <div class="jumbotron">
-    <h1>Navbar example</h1>
+    <h1>
+        <?php
+            echo $nome_usuario; 
+        ?></h1>
     <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
     <p>
         <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
@@ -85,9 +88,6 @@
     </div> <!-- /container -->
 
 
-    <?php
-        echo "<h3>Bem vindo: " . $nome_usuario . "</h3>"; 
-    ?>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
