c1063bd459a66f348835a032628c6c6db2aa8b62
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-09-02 01:50:22 -0300

    Exercicio teste

--- usuario_gravar.php
+++ usuario_gravar.php
@@ -19,6 +19,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio SysPacientes</title>
+    <link rel="icon" href="img/favicon/favicon2.ico">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <?php 
