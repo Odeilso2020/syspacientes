a12460777bc785e889c0200df570160df876dfcc
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-09-02 01:08:49 -0300

    .

--- usuario.php
+++ usuario.php
@@ -4,6 +4,20 @@
     require_once('conexao.php');
 
     $idUsuario = $_GET["idUsuario"];
+    $nomeUsuario = "";
+    $emailUsuario = "";
+    $senhaUsuario = "";
+
+    $sql = "SELECT nome, email, senha FROM usuarios WHERE id=" . $idUsuario;
+    $resp = mysqli_query($conexao_bd, $sql);
+    if($rows=mysqli_fetch_row($resp))
+    {
+        $nomeUsuario = $rows[0];
+        $emailUsuario = $rows[1];
+        $senhaUsuario = $rows[2];
+    }
+
+    mysqli_close($conexao_bd);
 ?>
 
 <!DOCTYPE html>
@@ -17,13 +31,13 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Editando o Usuário XXX</h1>
+    <h1>Editando o Usuário <?php echo($nomeUsuario); ?></h1>
     <hr>
     <div class="container">
         <form>
             <div class="form-group">
                 <label for="inputNome">Nome do Usuário:</label>
-                <input type="text" class="form-control" id="inputNome" name="inputNome" placeholder="Nome do usuario">
+                <input type="text" class="form-control" id="inputNome" name="inputNome" placeholder="Nome do usuario" value="<?php echo($nomeUsuario); ?>">
             </div>
             <div class="form-group">
                 <label for="inputEmail">E-mail: </label>
