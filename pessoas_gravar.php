d30efb9ee6453eec3a95949a056140bf319677ab
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-10-02 01:30:36 -0300

    testeEdição

--- pessoas_gravar.php
+++ pessoas_gravar.php
@@ -16,11 +16,31 @@
    $celular     = $_POST["inputCelular"];  
    $emailPessoa = $_POST["inputEmail"];
 
-   
-      $sql = "INSERT INTO pessoas( nome, endereco, numero, complemento, cidade, estado, cep, datanascimento, telefone, celular, email)
-      VALUES('$nomePessoa', '$endereco', '$numero','$complemento', '$cidade', '$estado',  '$cep', '$datanasc', '$telefone', '$celular', '$emailPessoa')";
+   if(strlen($idPessoa) > 0){
+      if($idPessoa != 0){
+         //atualizar
+         $sql = "UPDATE pessoas SET 
+                  nome = '$nome', 
+                  endereco = '$endereco',
+                  numero = '$numero',
+                  complemento = '$complemento',
+                  cidade = '$cidade',
+                  estado = '$estado',
+                  cep = '$cep',
+                  datanascimento = '$datanasc',
+                  telefone = '$telefone',
+                  celular = '$celular',
+                  email = '$email'
+                 WHERE id = $idPessoa";
+      } else {
+         $sql = "INSERT INTO pessoas( nome, endereco, numero, complemento, cidade, estado, cep, datanascimento, telefone, celular, email)
+         VALUES('$nomePessoa', '$endereco', '$numero','$complemento', '$cidade', '$estado',  '$cep', '$datanasc', '$telefone', '$celular', '$emailPessoa')";
+      }
+      
       mysqli_query($conexao_bd, $sql);
-   
+   } else {
+      // erro
+   }
 
    mysqli_close($conexao_bd);
    header("location:pessoas_list.php");
