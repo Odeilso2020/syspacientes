10cbc9b0dc4d3014f5735e4814016b5f52ea0276
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-10-01 22:25:05 -0300

    testeExcluir

--- pessoas_excluir.php
+++ pessoas_excluir.php
@@ -7,9 +7,12 @@
 
     
    //echo $idPessoa;
+   if(strlen($idPessoa) > 0){
+      $sql = "DELETE FROM pessoas WHERE id =" .$idPessoa ;
+      mysqli_query($conexao_bd, $sql);
+   }
    //verifico se é vazio:
-   $sql = "DELETE FROM pessoas WHERE id =" .$idPessoa ;
-   mysqli_query($conexao_bd, $sql);
+   
   
    mysqli_close($conexao_bd);
    header("location:pessoas_list.php");
